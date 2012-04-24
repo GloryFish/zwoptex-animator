f7274b4b56a4a233f84138c7832cd62a462c79ec
Jay Roberts <jay@gloryfish.org>
2012-04-23 22:10:54 -0400

    Update animation template

--- loveapp/resources/templates/animations.lua
+++ loveapp/resources/templates/animations.lua
@@ -9,7 +9,35 @@ local animations = {
       -- Add new frames here
     }
   },
-  -- Add new animations here
+  player_man_walking = {
+    frames = {
+      {
+        name = 'player_man_walk_1', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_2', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_3', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_4', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_5', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_6', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },  -- Add new animations here
 }
 
 return animations
