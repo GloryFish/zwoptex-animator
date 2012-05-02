ee44f73bfffb6711048310af6f4b3b25ab832156
Paul Bredenberg <paulb@designhammer.com>
2012-05-01 22:15:52 -0400

    add print path name

--- loveapp/spritesheets.lua
+++ loveapp/spritesheets.lua
@@ -23,6 +23,7 @@ function Spritesheet:hasSpriteNamed(spritename)
 end
 
 function Spritesheet:load(path, name)
+	print(path..name..'.png')
   self.texture = love.graphics.newImage(path..name..'.png') 
   self.texture:setFilter('nearest', 'nearest')
   
